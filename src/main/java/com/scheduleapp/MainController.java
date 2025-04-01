010ba6f94223ed7c695bfe8e6feb1228959cf741
camil-gh <kamilost99@gmail.com>
2025-04-01 23:47:54 +0200

    0.3

--- src/main/java/com/scheduleapp/MainController.java
+++ src/main/java/com/scheduleapp/MainController.java
@@ -1,6 +1,9 @@
 package com.scheduleapp;
 
+import com.scheduleapp.services.KierunkiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,8 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String index(){
-        return"index";
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/login")
@@ -22,5 +25,20 @@ public class MainController {
     public String register() {
         // Zwrócenie widoku książek (np. szablon Thymeleaf)
         return "register"; // To odpowiada plikowi books.html w src/main/resources/templates
+    }
+
+    @GetMapping("/schedule")
+    public String schedule() {
+        // Zwrócenie widoku książek (np. szablon Thymeleaf)
+        return "schedule"; // To odpowiada plikowi books.html w src/main/resources/templates
+    }
+
+    @Autowired
+    private KierunkiService kierunkiService;
+
+    @GetMapping("/kierunki")
+    public String getKierunki(Model model) {
+        model.addAttribute("kierunki", kierunkiService.getAllKierunki());
+        return "kierunki";  // Nazwa szablonu HTML
     }
 }
